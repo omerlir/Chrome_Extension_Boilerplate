97ae84cce00ddf465dfce1824baa231f6a25c166
omerlir <133787053+omerlir@users.noreply.github.com>
2024-04-18 16:33:53 +0300

    add css loader

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
   },
   module: {
     rules: [
@@ -15,22 +16,27 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
       },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+      },
+      {
+        type: 'asset/resource',
+        test: /\.(png|jpg|jpeg|png|woff|woff2|eot|svg|ttf|ico|json)$/i,
+      },
     ],
   },
   plugins: [
     new copyPlugin({
       patterns: [
         {
-          from: path.resolve('src/manifest.json'),
+          from: path.resolve('src/static'),
           to: path.resolve('dist'),
         },
       ],
     }),
-    new HtmlPlugin({
-      title: 'Project',
-      filename: 'popup.html',
-      chunks: ['popup'],
-    }),
+    getHtmlPlugin('popup'),
+    getHtmlPlugin('options'),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -39,4 +45,17 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
+
+function getHtmlPlugin(chunks) {
+  return new HtmlPlugin({
+    title: 'Project',
+    filename: chunks + '.html',
+    chunks: [chunks],
+  })
 }
